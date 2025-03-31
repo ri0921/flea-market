c948d3e284f770d33f1ce7ecf04a17da7d458a26
rina <yitenglinai69@gmail.com>
2025-03-31 16:15:38 +0900

    購入画面の実装

--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -6,51 +6,52 @@
 
 @section('content')
 <div class="main">
-    <form class="order__form" action="" method="post">
-    @csrf
-        <div class="order-details">
-            <div class="order__group">
-                <div class="item-image">
-                    <img src="{{ asset('test-image.png') }}" alt="商品画像" width="100%">
-                </div>
-                <p class="item-name">商品名</p>
-                <div class="item-price"><span>¥</span>47,000</div>
+    <div class="order-details">
+        <div class="order__group">
+            <div class="item-image">
+                <img src="{{ asset($item->image) }}" alt="商品画像" width="100%">
             </div>
-            <div class="order__group">
-                <p class="order__group-p">支払い方法</p>
-                <div class="form__select">
-                    <select class="payment-method" name="payment">
-                        <option value="" selected>選択してください</option>
-                        <option value="コンビニ払い">コンビニ払い</option>
-                        <option value="カード支払い">カード支払い</option>
-                    </select>
+            <p class="item-name">{{ $item->name }}</p>
+            <div class="item-price"><span>¥</span>{{ number_format($item->price) }}</div>
+        </div>
+        <div class="order__group">
+            <p class="order__group-p">支払い方法</p>
+            <form class="form__select" action="/purchase/:{{ $item->id }}" method="get">
+                @csrf
+                <select class="payment-method" name="payment" onchange="submit(this.form)">
+                    <option value="" {{ old('payment', session('payment')) == '' ? 'selected' : '' }}>選択してください</option>
+                    <option value="コンビニ払い" {{ old('payment', session('payment')) == 'コンビニ払い' ? 'selected' : '' }}>コンビニ払い</option>
+                    <option value="カード支払い" {{ old('payment', session('payment')) == 'カード支払い' ? 'selected' : '' }}>カード支払い</option>
+                </select>
+            </form>
+        </div>
+        <div class="order__group">
+            <div class="group__row">
+                <p class="order__group-p">配送先</p>
+                <a href="/purchase/address/:{{ $item->id }}">変更する</a>
                 </div>
-            </div>
-            <div class="order__group">
-                <div class="group__row">
-                    <p class="order__group-p">配送先</p>
-                    <a href="/purchase/address">変更する</a>
-                </div>
-                <div class="destination">
-                    <div class="post_code">〒XXX-YYYY</div>
-                    <div class="address">ここには住所と</div>
-                    <div class="building">建物が入ります。</div>
-                </div>
+            <div class="destination">
+                <div class="post_code">{{ $address['post_code'] }}</div>
+                <div class="address">{{ $address['address'] }}</div>
+                <div class="building">{{ $address['building'] }}</div>
             </div>
         </div>
-        <div class="order-confirm">
+    </div>
+    <div class="order-confirm">
+        <form action="" method="post">
+            @csrf
             <table>
                 <tr>
                     <th>商品代金</th>
-                    <td><span>¥</span>47,000</td>
+                    <td><span>¥</span>{{ number_format($item->price) }}</td>
                 </tr>
                 <tr>
                     <th>支払い方法</th>
-                    <td>コンビニ払い</td>
+                    <td>{{ session('payment') ?? '選択されていません' }}</td>
                 </tr>
             </table>
             <button class="purchase-button" type="submit">購入する</button>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
 @endsection
