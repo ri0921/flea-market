7391d68b8ed627375e83215b0d5905fec3c3120c
rina <yitenglinai69@gmail.com>
2025-04-13 10:07:36 +0900

    マイページの取得

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -16,20 +16,37 @@
         </div>
     </div>
     <div class="tabs">
-        <a class="tab-button" href="/mypage?tab=sell">出品した商品</a>
-        <a class="tab-button" href="/mypage?tab=buy">購入した商品</a>
+        <a class="tab-button {{ $tab === 'sell' ? 'active' : '' }}" href="/mypage?tab=sell">出品した商品</a>
+        <a class="tab-button {{ $tab === 'buy' ? 'active' : '' }}" href="/mypage?tab=buy">購入した商品</a>
     </div>
     <div class="tab-content">
+        @if ($tab === 'sell' && $sellItems->isNotEmpty())
         <ul class="list">
-            @foreach ($items as $item)
+            @foreach ($sellItems as $item)
             <li class="list-card">
-                <a href="/item/{{ $item->id }}">
+                <a class="item-link" href="/item/{{ $item->id }}">
                     <img class="card__image" src="{{ Storage::url($item->image) }}" alt="商品画像" width="100%">
+                    @if($item->is_sold)
+                        <div class="sold-stamp">Sold</div>
+                    @endif
                     <p class="card__title">{{ $item->name }}</p>
                 </a>
             </li>
             @endforeach
         </ul>
+        @elseif ($tab === 'buy' && $buyItems->isNotEmpty())
+        <ul class="list">
+            @foreach ($buyItems as $purchase)
+            <li class="list-card">
+                <a class="item-link" href="/item/{{ $purchase->item->id }}">
+                    <img class="card__image" src="{{ Storage::url($purchase->item->image) }}" alt="商品画像" width="100%">
+                    <div class="sold-stamp">Sold</div>
+                    <p class="card__title">{{ $purchase->item->name }}</p>
+                </a>
+            </li>
+            @endforeach
+        </ul>
+        @endif
     </div>
 </div>
 @endsection
