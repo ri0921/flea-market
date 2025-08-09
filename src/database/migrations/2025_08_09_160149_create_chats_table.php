6ef23328ef9db1bc8a237dc4f815d79372a19ab7
rina <yitenglinai69@gmail.com>
2025-08-09 16:07:25 +0900

    chatsテーブルの追加

---
+++ src/database/migrations/2025_08_09_160149_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id')->constrained()->cascadeOnDelete();
+            $table->text('message');
+            $table->datetime('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
